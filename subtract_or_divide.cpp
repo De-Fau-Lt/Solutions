984bc00a46f955bd5561f8a51a2f72c8cd304af4
De-Fau-Lt <prayagrai.93@gmail.com>
2020-11-26 15:27:45 +0530

    codeforces

---
+++ subtract_or_divide.cpp
@@ -0,0 +1,46 @@
+/*
+ID: prayagr1
+TASK:
+LANG: C++
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	if(n==1)
+		cout << 0;
+	else if(n==2)
+		cout << 1;
+	else if(n==3)
+		cout << 2;
+	else if(n&1)
+		cout << 3;
+	else cout << 2;
+}
+
+int main() 
+{ 
+	ios_base::sync_with_stdio(false);cin.tie(NULL); 
+
+		#ifndef ONLINE_JUDGE 
+	//freopen("input.txt", "r", stdin); 
+	//freopen("error.txt", "w", stderr); 
+	//freopen("output.txt", "w", stdout);
+  	#endif
+
+	int t=1; 
+	/*is Single Test case?*/cin>>t; 
+	while(t--) 
+	{ 
+		solve(); 
+		cout<<"\n";
+	} 
+
+	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl; 
+	return 0; 
+}
