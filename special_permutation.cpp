da48a3924e99f8f3e4b621804918d791a95aca2c
De-Fau-Lt <prayagrai.93@gmail.com>
2020-11-26 19:36:55 +0530

    codeforces

---
+++ special_permutation.cpp
@@ -0,0 +1,50 @@
+/*
+ID: prayagr1
+TASK:
+LANG: C++
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int n;
+	cin >> n;
+	int n2=n;
+	
+	int arr[n];
+
+	for(int i=0;i<n2;i++)
+	{
+		arr[i]=n--;
+	}
+
+	if(n2&1)
+		swap(arr[n2/2],arr[n2-1]);
+
+	for(auto x:arr)
+		cout << x << " ";
+}
+
+int main() 
+{ 
+	ios_base::sync_with_stdio(false);cin.tie(NULL); 
+
+		#ifndef ONLINE_JUDGE 
+	//freopen("input.txt", "r", stdin); 
+	//freopen("error.txt", "w", stderr); 
+	//freopen("output.txt", "w", stdout);
+  	#endif
+
+	int t=1; 
+	/*is Single Test case?*/cin>>t; 
+	while(t--) 
+	{ 
+		solve(); 
+		cout<<"\n";
+	} 
+
+	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl; 
+	return 0; 
+}
