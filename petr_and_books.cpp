119241f8594954ce687d15c35324b7c8ddc50a5c
De-Fau-Lt <prayagrai.93@gmail.com>
2020-11-30 17:51:59 +0530

    codeforces

---
+++ petr_and_books.cpp
@@ -0,0 +1,49 @@
+/*
+ID: prayagr1
+TASK:
+LANG: C++
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	int arr[7];
+	for(auto &x:arr)
+		cin >> x;
+	
+	int i=0;
+	while(n > 0)
+	{
+		if(i==7)
+			i=0;
+		n-=arr[i++];
+	}
+	cout << i;
+}
+
+int main() 
+{ 
+	ios_base::sync_with_stdio(false);cin.tie(NULL); 
+
+		#ifndef ONLINE_JUDGE 
+	//freopen("input.txt", "r", stdin); 
+	//freopen("error.txt", "w", stderr); 
+	//freopen("output.txt", "w", stdout);
+  	#endif
+
+	int t=1; 
+	//*is Single Test case?*/cin>>t; 
+	while(t--) 
+	{ 
+		solve(); 
+		cout<<"\n";
+	} 
+
+	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl; 
+	return 0; 
+}
