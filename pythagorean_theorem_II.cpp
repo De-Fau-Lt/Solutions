67ab3d632cd8e6673cd7e00210a7a7b108dbd0fb
De-Fau-Lt <prayagrai.93@gmail.com>
2020-12-08 16:40:56 +0530

    codeforeces

---
+++ pythagorean_theorem_II.cpp
@@ -0,0 +1,50 @@
+/*
+ID: prayagr1
+TASK:
+LANG: C++
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int n;
+	cin >> n;
+	
+	int cnt=0;
+	for(int a=1;a<=n;a++)
+	{
+		for(int b=a;b<=n;b++)
+		{
+			int c=(a*a)+(b*b);
+			int sq = sqrt(c);
+
+			if(sq*sq == c && sq <= n)
+				cnt++;
+		}
+	}
+	cout << cnt;
+}
+
+int main() 
+{ 
+	ios_base::sync_with_stdio(false);cin.tie(NULL); 
+
+		#ifndef ONLINE_JUDGE 
+	//freopen("input.txt", "r", stdin); 
+	//freopen("error.txt", "w", stderr); 
+	//freopen("output.txt", "w", stdout);
+  	#endif
+
+	int t=1; 
+	//*is Single Test case?*/cin>>t; 
+	while(t--) 
+	{ 
+		solve(); 
+		cout<<"\n";
+	} 
+
+	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl; 
+	return 0; 
+}
