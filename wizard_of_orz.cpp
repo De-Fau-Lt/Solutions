81dc09b86ebdd50046ffa20de2a1b5bbcce60741
De-Fau-Lt <prayagrai.93@gmail.com>
2021-01-17 19:05:43 +0530

    codeforces

---
+++ wizard_of_orz.cpp
@@ -0,0 +1,52 @@
+/*
+ID: prayagr1
+TASK:
+LANG: C++
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int n;
+	cin >> n;
+	
+	if(n == 1)
+	{
+		cout << 9;
+		return;
+	}
+
+	cout << 9 << 8;
+
+	int c=9;
+	for(int i=0;i<n-2;i++)
+	{
+		cout << c++;
+		if(c>9) c=0;
+	}
+
+}
+
+int main() 
+{ 
+	ios_base::sync_with_stdio(false);cin.tie(NULL); 
+
+		#ifndef ONLINE_JUDGE 
+	//freopen("input.txt", "r", stdin); 
+	//freopen("error.txt", "w", stderr); 
+	//freopen("output.txt", "w", stdout);
+  	#endif
+
+	int t=1; 
+	/*is Single Test case?*/cin>>t; 
+	while(t--) 
+	{ 
+		solve(); 
+		cout<<"\n";
+	} 
+
+	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl; 
+	return 0; 
+}
